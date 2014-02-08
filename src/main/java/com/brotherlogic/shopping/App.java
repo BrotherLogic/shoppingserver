665a025ebf1df7a8fcf753837634d7d7fbefded6
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2014-02-08 08:10:55 -0800

    Added base layout

---
+++ src/main/java/com/brotherlogic/shopping/App.java
@@ -0,0 +1,13 @@
+package com.brotherlogic.shopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
